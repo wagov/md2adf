ff2d0202d54de6fc995e77cc692172c5d9ac1216
Adon Metcalfe <adonm@fastmail.fm>
2022-07-06 07:42:32 +0800

    basics work

--- md2adf/index.ts
+++ md2adf/index.ts
@@ -1,15 +1,14 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { MarkdownTransformer } from '@atlaskit/editor-markdown-transformer';
+import { JSONTransformer } from '@atlaskit/editor-json-transformer';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const transformer = new MarkdownTransformer(); // default schema
+    const serializer = new JSONTransformer();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: serializer.encode(transformer.parse(req.body)) // return parsed markdown converted to atlassian document format
     };
 
 };
