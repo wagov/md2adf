184f5f8a64cf703d455f21cd4fb8294eb76f36a3
Adon Metcalfe <adonm@fastmail.fm>
2022-07-07 11:06:57 +0800

    added content-type hint

--- md2adf/index.ts
+++ md2adf/index.ts
@@ -8,7 +8,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: serializer.encode(transformer.parse(req.body)) // return parsed markdown converted to atlassian document format
+        body: serializer.encode(transformer.parse(req.body)), // return parsed markdown converted to atlassian document format
+        headers: { 'Content-Type': 'application/json' }
     };
 
 };
